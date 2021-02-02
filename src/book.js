20a8b9c328be7998d943d8ce47ce440df58ad0c0
Ben Firth <bfirth93@gmail.com>
2021-02-02 16:44:53 -0700

    Finish up book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
-}
+const createTitle = (title) => {
+  return `The ${title}`;
+};
+
+const buildMainCharacter = (name, age, pronouns) => {
+  const charInfo = {
+    name,
+    age,
+    pronouns
+  };
+
+  return charInfo;
+};
+
+const saveReview = (review, allReviews) => {
+  if (!allReviews.includes(review)) allReviews.push(review);
+
+  return allReviews;
+};
+
+const calculatePageCount = (title) => {
+  return title.length * 20;
+};
+
+const writeBook = (title, mainCharacter, genre) => {
+  const book = {
+    title,
+    mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre
+  };
+
+  return book;
+};
+
+const editBook = (book) => {
+  book.pageCount = book.pageCount * 0.75;
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
