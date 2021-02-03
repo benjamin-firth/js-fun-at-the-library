ca1b73b2b15cde69c9dcfcf9f970c54c5e211f48
Ben Firth <bfirth93@gmail.com>
2021-02-02 17:57:14 -0700

    Add library solutions

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,33 @@
+const createLibrary = (name) => {
+  return { 
+    name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  };
+};
 
+const addBook = (library, book) => {
+  library.shelves[book.genre].push(book);
+};
+
+const checkoutBook = (library, bookTitle, shelf) => {
+  const bookIndex = library.shelves[shelf]
+    .findIndex(book => book.title === bookTitle);
+
+  if (bookIndex === -1) {
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+  } else {
+    library.shelves[shelf].splice(bookIndex, 1);
+  
+    return `You have now checked out ${bookTitle} from the ${library.name}`;
+  };
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
