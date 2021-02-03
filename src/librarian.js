762781ef4ad8d9577aba1932a113ebda26c1fd7f
Ben Firth <bfirth93@gmail.com>
2021-02-03 10:00:17 -0700

    Add librarian solution

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,34 @@
 class Librarian {
-  constructor() {
-    
-  }
-}
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
+
+  greetPatron(name, isMorning) {
+    return isMorning ? `Good morning, ${name}!` : `Hello, ${name}!`;
+  };
+
+  findBook(bookName) {
+    const foundBook = Object.keys(this.library.shelves).reduce((hasBook, shelf) => {
+      let bookIndex = this.library.shelves[shelf]
+        .findIndex(book => book.title === bookName);
+
+      if (bookIndex !== -1) {
+        this.library.shelves[shelf].splice(bookIndex, 1);
+        return hasBook = true;
+      };
+
+      return hasBook;
+    }, false);
+
+    return foundBook ?
+      `Yes, we have ${bookName}`: 
+      `Sorry, we do not have ${bookName}`;
+  };
+
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * 0.25);
+  };
+};
 
 module.exports = Librarian;
