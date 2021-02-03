7897e667bc65abbca4558a5c288098b38f426814
Ben Firth <bfirth93@gmail.com>
2021-02-02 17:37:50 -0700

    Finish shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,22 @@
+const shelfBook = (book, shelf) => {
+  if (shelf.length < 3) shelf.unshift(book);
+};
+
+const unshelfBook = (name, shelf) => {
+  shelf.splice(shelf.findIndex(book => book.title === name), 1);
+};
+
+const listTitles = (shelf) => {
+  return shelf.map(book => book.title).join(', ');
+};
+
+const searchShelf = (shelf, name) => {
+  return listTitles(shelf).includes(name);
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
